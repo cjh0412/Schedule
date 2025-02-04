7e95b1319aee13ca07e6f41b9e334a50284567e5
jyedev04 <jyedev04@gmail.com>
2025-02-04 13:44:58 +0900

    impl 일정 update 수정

--- src/main/java/com/example/todo/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/todo/schedule/controller/ScheduleController.java
@@ -3,6 +3,7 @@ package com.example.todo.schedule.controller;
 import com.example.todo.schedule.dto.ScheduleRequestDto;
 import com.example.todo.schedule.dto.ScheduleResponseDto;
 import com.example.todo.schedule.service.ScheduleService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,9 +26,7 @@ public class ScheduleController {
     @PostMapping
     public ResponseEntity<ScheduleResponseDto> createSchedule(
             @RequestBody @Valid ScheduleRequestDto dto){
-        return new ResponseEntity<>(
-                scheduleService.saveSchedule(dto),
-                HttpStatus.CREATED);
+        return new ResponseEntity<>(scheduleService.saveSchedule(dto), HttpStatus.CREATED);
     }
 
     @GetMapping
@@ -48,9 +47,8 @@ public class ScheduleController {
     @PatchMapping("{id}")
     public ResponseEntity<ScheduleResponseDto> updateSchedule(
             @PathVariable Long id
-            , @RequestBody ScheduleRequestDto dto
-            , @RequestParam String author) {
-        return new ResponseEntity<>(scheduleService.updateSchedule(id, dto.getTitle(), dto.getContents(), author, dto.getPassword()), HttpStatus.OK);
+            , @RequestBody ScheduleRequestDto dto) {
+        return new ResponseEntity<>(scheduleService.updateSchedule(id, dto.getTitle(), dto.getContents(), dto.getAuthorRequestDto().getName(), dto.getPassword()), HttpStatus.OK);
     }
 
     @PutMapping("{id}")
